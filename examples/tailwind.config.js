4d86c46630c8e963ba0005f032a8125b5ae48a25
qpwoei0123 <qpwoei0123@naver.com>
2026-02-19 14:39:56 +0900

    feat(preview): 프리뷰 페이지 갤러리와 제어 UI 추가

--- examples/tailwind.config.js
+++ examples/tailwind.config.js
@@ -1,7 +1,22 @@
+const motionKit = require('../src');
+
+const tmk = motionKit();
+
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        ...tmk.presets.fade.keyframes,
+        ...tmk.presets.slide.keyframes,
+        ...tmk.presets.scale.keyframes,
+      },
+      animation: {
+        ...tmk.presets.fade.animations,
+        ...tmk.presets.slide.animations,
+        ...tmk.presets.scale.animations,
+      },
+    },
   },
   plugins: [],
 };
